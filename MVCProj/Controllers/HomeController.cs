8d85bd91f146065555abbbe7e0887f17e85ccc70
janluksoft <janluksoft@interia.pl>
2022-04-14 14:18:37 +0200

    Add project files.

---
+++ MVCProj/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+//using MVCProj.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Page2()
+        {
+            return View();
+        }
+
+        public IActionResult TagHelper()
+        {
+            return View();
+        }
+
+    }
+}
